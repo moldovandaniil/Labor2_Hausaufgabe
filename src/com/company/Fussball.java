f0854d5a0b25529cf3611b6a2101f14bbca75627
moldovandaniil <91666610+moldovandaniil@users.noreply.github.com>
2021-10-21 13:20:23 +0300

    Update Fussball.java

--- src/com/company/Fussball.java
+++ src/com/company/Fussball.java
@@ -1,5 +1,10 @@
 package com.company;
 
+/**
+* Creating the Class Fussball 
+*   which extends Mannschaftssport
+*       and returns 65
+*/
 public class Fussball extends  Mannschaftssport {
 
     @Override
