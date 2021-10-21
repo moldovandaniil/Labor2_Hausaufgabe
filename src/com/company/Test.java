9cf239768fbe4bd86968042f61e1a10b58715f89
moldovandaniil <91666610+moldovandaniil@users.noreply.github.com>
2021-10-21 13:25:15 +0300

    Update Test.java

--- src/com/company/Test.java
+++ src/com/company/Test.java
@@ -1,29 +1,45 @@
 package com.company;
+
+/**
+* Importing junit for assert tests
+*/ 
 import static org.junit.Assert.assertEquals;
 import org.junit.*;
 
+/**
+* Importing Java Collections for List usage
+*/ 
 import java.util.ArrayList;
 import java.util.List;
+
+
 
 public class Test {
 
     @org.junit.Test
     public void mainTest()
     {
-        //setting test parameters and values
+        /**
+        * Creating test instances of each class
+        */ 
+        
         List<Sport> l1 = new ArrayList<Sport>();
         Sport s1 = new Fussball();
         Sport s2 = new Hochsprung();
         Sport s3 = new Fussball();
-        l1.add(s1);
+        
+        l1.add(s1);                                                        /* Adding to the list the created sports*/
         l1.add(s2);
         l1.add(s3);
-        Benutzer b1 = new Benutzer("Andrei", "Maruta", l1);
+        
+        Benutzer b1 = new Benutzer("Andrei", "Maruta", l1);                /*Creating an user */
 
-        //assertEquals tests
-        assertEquals(150.0, b1.kalkuliereZeit(), 0.0);
-        assertEquals(130.0, b1.kalkuliereZeit(new Fussball()), 0.0);
-        assertEquals(50.0, b1.kalkuliereDurschnittzeit(), 0.0);
+        /**
+        * Testing the overall functionality with assert tests
+        */
+        assertEquals(150.0, b1.kalkuliereZeit(), 0.0);                     //True
+        assertEquals(130.0, b1.kalkuliereZeit(new Fussball()), 0.0);       //True
+        assertEquals(50.0, b1.kalkuliereDurschnittzeit(), 0.0);            //True
 
     }
 
